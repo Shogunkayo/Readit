da7a8f3e92806de6bd84772779dcecbb9c44fba1
Shogunkayo <pyrotourface@gmail.com>
2023-11-22 23:17:44 +0530

    updated db schema to v2

--- database/create_schema.sql
+++ database/create_schema.sql
@@ -1,117 +1,228 @@
 CREATE DATABASE readit;
 USE readit;
 
-CREATE TABLE Company(
-            company_id varchar(15),
-            company_name varchar(30),
-            PRIMARY KEY (company_id));
+CREATE TABLE Country (
+    country_id INT NOT NULL AUTO_INCREMENT,
+    country_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (country_id)
+);
 
-CREATE TABLE Institution(
-            i_id varchar(15),
-            i_name varchar(30),
-            PRIMARY KEY (i_id));
+CREATE TABLE City (
+    city_id INT NOT NULL AUTO_INCREMENT,
+    city_name VARCHAR(50) NOT NULL,
+    country_id INT NOT NULL,
+    PRIMARY KEY (city_id),
+    FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
 
-CREATE TABLE Researcher(
-            r_id varchar(15),
-            email nvarchar(30),
-            last_name varchar(30),
-            passwd varchar(30),
-            PRIMARY KEY (r_id));
+CREATE TABLE Researcher
+(
+    r_id UUID NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    password BINARY(60) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    h_index INT NOT NULL DEFAULT 0,
+    no_citations INT NOT NULL DEFAULT 0,
+    date_created DATETIME NOT NULL,
+    page_visits INT NOT NULL DEFAULT 0,
+    nationality VARCHAR(20) NOT NULL,
+    PRIMARY KEY (r_id),
+    UNIQUE (email)
+);
 
-CREATE TABLE Paper(
-            doi varchar(15),
-            no_reads int,
-            blob_paper blob,
-            blob_abstract blob,
-            title varchar(30),
-            PRIMARY KEY (doi));
+CREATE TABLE Paper
+(
+    doi UUID NOT NULL,
+    domain VARCHAR(20) NOT NULL,
+    blob_paper LONGBLOB NOT NULL,
+    blob_abstract MEDIUMBLOB NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    no_reads INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (doi)
+);
 
-CREATE TABLE Conference(
-            c_id varchar(15),
-            tier int,
-            c_name varchar(30),
-            date_conduct date,
-            venue_conduct varchar(40),
-            PRIMARY KEY (c_id));
+CREATE TABLE Organization
+(
+    o_id UUID NOT NULL,
+    o_name VARCHAR(20) NOT NULL,
+    location VARCHAR(20) NOT NULL,
+    PRIMARY KEY (o_id)
+);
 
-CREATE TABLE Company_team(
-            team_id varchar(15),
-            company_id varchar(15),
-            team_name varchar(30),
-            PRIMARY KEY (team_id, company_id),
-            FOREIGN KEY (company_id) REFERENCES Company(company_id));
+CREATE TABLE Private_Repository
+(
+    repo_id UUID NOT NULL,
+    repo_name VARCHAR(20) NOT NULL,
+    admin_id UUID NOT NULL,
+    PRIMARY KEY (repo_id),
+    FOREIGN KEY (admin_id) REFERENCES Researcher(r_id)
+);
 
-CREATE TABLE Department(
-            i_id varchar(15),
-            department_id varchar(15),
-            domain varchar(30),
-            PRIMARY KEY (i_id, department_id),
-            FOREIGN KEY (i_id) REFERENCES Institution(i_id));
+CREATE TABLE University
+(
+    u_id UUID NOT NULL,
+    u_name VARCHAR(20) NOT NULL,
+    city_id INT NOT NULL,
+    country_id INT NOT NULL,
+    accredition VARCHAR(20) NOT NULL,
+    graduation_rate INT NOT NULL,
+    PRIMARY KEY (u_id),
+    FOREIGN KEY (city_id) REFERENCES City(city_id),
+    FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
 
-CREATE TABLE RWorkinC(
-            role varchar(30),
-            r_id varchar(15),
-            team_id varchar(15),
-            company_id varchar(15),
-            FOREIGN KEY (team_id, company_id) REFERENCES Company_team(team_id, company_id),
-            FOREIGN KEY (r_id) REFERENCES Researcher(r_id));
+CREATE TABLE Company
+(
+    c_id UUID NOT NULL,
+    c_name VARCHAR(20) NOT NULL,
+    comp_type ENUM('NGO', 'For Profit', 'Other') NOT NULL,
+    industry_sector INT NOT NULL,
+    head_city_id INT NOT NULL,
+    market_share INT NOT NULL,
+    PRIMARY KEY (c_id),
+    FOREIGN KEY (head_city_id) REFERENCES City(city_id)
+);
 
-CREATE TABLE RWorkinI(
-            r_id varchar(15),
-            i_id varchar(15),
-            department_id varchar(15),
-            FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
-            FOREIGN KEY (i_id, department_id) REFERENCES Department(i_id, department_id));
+CREATE TABLE Department
+(
+    d_id UUID NOT NULL,
+    d_name VARCHAR(20) NOT NULL,
+    d_head UUID NOT NULL,
+    budget INT NOT NULL DEFAULT 0,
+    no_students INT NOT NULL DEFAULT 0,
+    u_id UUID NOT NULL,
+    PRIMARY KEY (d_id),
+    FOREIGN KEY (u_id) REFERENCES University(u_id),
+    FOREIGN KEY (d_head) REFERENCES Researcher(r_id)
+);
 
-CREATE TABLE Paper_keywords(
-            keywords varchar(30),
-            doi varchar(15),
-            PRIMARY KEY (keywords, doi),
-            FOREIGN KEY (doi) REFERENCES Paper(doi));
+CREATE TABLE Paper_keywords
+(
+    keyword VARCHAR(20) NOT NULL,
+    doi UUID NOT NULL,
+    PRIMARY KEY (keyword, doi),
+    FOREIGN KEY (doi) REFERENCES Paper(doi)
+);
 
-CREATE TABLE Organization(
-            o_id varchar(15),
-            o_name varchar(30),
-            c_id varchar(15),
-            PRIMARY KEY (o_id),
-            FOREIGN KEY (c_id) REFERENCES Conference(c_id));
+CREATE TABLE University_degrees
+(
+    degree VARCHAR(20) NOT NULL,
+    u_id UUID NOT NULL,
+    PRIMARY KEY (degree, u_id),
+    FOREIGN KEY (u_id) REFERENCES University(u_id)
+);
 
-CREATE TABLE Conference_domains(
-            domains varchar(30),
-            c_id varchar(15),
-            PRIMARY KEY (domains, c_id),
-            FOREIGN KEY (c_id) REFERENCES Conference(c_id));
+CREATE TABLE WritePaper
+(
+    r_id UUID NOT NULL,
+    doi UUID NOT NULL,
+    PRIMARY KEY (r_id, doi),
+    FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
+    FOREIGN KEY (doi) REFERENCES Paper(doi)
+);
 
-CREATE TABLE Researcher_details(
-            h_index int,
-            no_citations int,
-            page_visits int,
-            date_created date,
-            first_name varchar(30),
-            nationality varchar(30),
-            r_id varchar(15),
-            FOREIGN KEY (r_id) REFERENCES Researcher(r_id));
+CREATE TABLE Follows
+(
+    follows UUID NOT NULL,
+    follower UUID NOT NULL,
+    PRIMARY KEY (follows, follower),
+    FOREIGN KEY (follows) REFERENCES Researcher(r_id),
+    FOREIGN KEY (follower) REFERENCES Researcher(r_id)
+);
 
-CREATE TABLE Apply_for(
-            doi varchar(15),
-            c_id varchar(15),
-            date_submitted date,
-            accepted bit,
-            PRIMARY KEY (doi, c_id),
-            FOREIGN KEY (doi) REFERENCES Paper(doi),
-            FOREIGN KEY (c_id) REFERENCES Conference(c_id));
+CREATE TABLE Belong_To
+(
+    r_id UUID NOT NULL,
+    o_id UUID NOT NULL,
+    PRIMARY KEY (r_id),
+    FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
+    FOREIGN KEY (o_id) REFERENCES Organization(o_id)
+);
 
-CREATE TABLE Publish(
-            doi varchar(15),
-            c_id varchar(15),
-            date_accepted date,
-            PRIMARY KEY (doi, c_id),
-            FOREIGN KEY (doi) REFERENCES Paper(doi),
-            FOREIGN KEY (c_id) REFERENCES Conference(c_id));
-            
-CREATE TABLE Write1(
-            r_id varchar(15),
-            doi varchar(15),
-            PRIMARY KEY (r_id, doi),
-            FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
-            FOREIGN KEY (doi) REFERENCES Paper(doi));
+CREATE TABLE Citations
+(
+    paper_citing UUID NOT NULL,
+    paper_cited UUID NOT NULL,
+    PRIMARY KEY (paper_citing, paper_cited),
+    FOREIGN KEY (paper_citing) REFERENCES Paper(doi),
+    FOREIGN KEY (paper_cited) REFERENCES Paper(doi)
+);
+
+CREATE TABLE Publish_Repo
+(
+    doi UUID NOT NULL,
+    repo_id UUID NOT NULL,
+    PRIMARY KEY (doi),
+    FOREIGN KEY (doi) REFERENCES Paper(doi),
+    FOREIGN KEY (repo_id) REFERENCES Private_Repository(repo_id)
+);
+
+CREATE TABLE Company_Repo
+(
+    c_id UUID NOT NULL,
+    repo_id UUID NOT NULL,
+    PRIMARY KEY (repo_id),
+    FOREIGN KEY (c_id) REFERENCES Company(c_id),
+    FOREIGN KEY (repo_id) REFERENCES Private_Repository(repo_id)
+);
+
+CREATE TABLE University_Repo
+(
+    repo_id UUID NOT NULL,
+    u_id UUID NOT NULL,
+    PRIMARY KEY (repo_id),
+    FOREIGN KEY (repo_id) REFERENCES Private_Repository(repo_id),
+    FOREIGN KEY (u_id) REFERENCES University(u_id)
+);
+
+CREATE TABLE Work_Company
+(
+    r_id UUID NOT NULL,
+    c_id UUID NOT NULL,
+    PRIMARY KEY (r_id),
+    FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
+    FOREIGN KEY (c_id) REFERENCES Company(c_id)
+);
+
+CREATE TABLE Work_University
+(
+    r_id UUID NOT NULL,
+    d_id UUID NOT NULL,
+    PRIMARY KEY (r_id),
+    FOREIGN KEY (r_id) REFERENCES Researcher(r_id),
+    FOREIGN KEY (d_id) REFERENCES Department(d_id)
+);
+
+CREATE TABLE Conference
+(
+    c_id UUID NOT NULL,
+    tier ENUM('Tier 1', 'Tier 2', 'Tier 3', 'Other') NOT NULL,
+    c_name VARCHAR(20) NOT NULL,
+    date_conduct DATE NOT NULL,
+    venue_city_id INT NOT NULL,
+    venue_country_id INT NOT NULL,
+    o_id UUID NOT NULL,
+    PRIMARY KEY (c_id),
+    FOREIGN KEY (o_id) REFERENCES Organization(o_id),
+    FOREIGN KEY (venue_city_id) REFERENCES City(city_id),
+    FOREIGN KEY (venue_country_id) REFERENCES Country(country_id)
+);
+
+CREATE TABLE Conference_domains
+(
+    domain VARCHAR(20) NOT NULL,
+    c_id UUID NOT NULL,
+    PRIMARY KEY (domain, c_id),
+    FOREIGN KEY (c_id) REFERENCES Conference(c_id)
+);
+
+CREATE TABLE Publish_Conf
+(
+    date_published DATETIME NOT NULL,
+    doi UUID NOT NULL,
+    c_id UUID NOT NULL,
+    PRIMARY KEY (doi),
+    FOREIGN KEY (doi) REFERENCES Paper(doi),
+    FOREIGN KEY (c_id) REFERENCES Conference(c_id)
+);
